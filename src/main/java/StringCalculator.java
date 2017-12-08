f28c7189777b7a2e6cf7802cec60108589e05c4d
Wojtek Pietrucha <wojtekpietrucha@gmail.com>
2017-12-08 23:49:09 +0100

    Initial commit

---
+++ src/main/java/StringCalculator.java
@@ -0,0 +1,8 @@
+public class StringCalculator {
+
+    public static final String EMPTY_STRING = "";
+
+    public int Add(String numbers) {
+        return 0;
+    }
+}
---
+++ src/test/java/StringCalculatorTest.java
@@ -0,0 +1,18 @@
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Tdd training on 09.12.17.
+ */
+
+public class StringCalculatorTest {
+
+    @Test
+    public void shouldCreateObject() throws Exception {
+        StringCalculator testedObject = new StringCalculator();
+        assertThat(testedObject).isNotNull();
+        assertThat(testedObject.Add("")).isEqualTo(0);
+    }
+
+}
