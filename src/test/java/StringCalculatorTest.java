bd55748e6b72b23db6d35688859432ae43f1ad24
MacBook Pro Magda <magda@ssolutions.com.pl>
2017-12-09 14:41:55 +0100

    JUnit

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -1,15 +1,29 @@
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import junitparams.naming.TestCaseName;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * Tdd training on 09.12.17.
  */
-
+@RunWith(JUnitParamsRunner.class)
 public class StringCalculatorTest {
-
+    
     StringCalculator testedObject;
+
+    private Object[][] parametersForTestingTwoNumbersInInput() {
+        return new Object[][] {
+                {"1,2", 3},
+                {"1,0", 1},
+                {"2,3,15", 20},
+                {"", 0},
+                {"1", 1},
+        };
+    }
 
     @Before
     public void setUp(){
@@ -22,32 +36,12 @@ public class StringCalculatorTest {
     }
 
     @Test
-    public void shouldReturnZeroWhenEmptyStringPassed() {
-        int result = testedObject.Add("");
+    @Parameters(method = "parametersForTestingTwoNumbersInInput")
+    @TestCaseName("Should return {1} when {0} passed")
+    public void shouldReturnSumWhenTwoNumbersPassed(String inPut, int value) {
+        int result = testedObject.Add(inPut);
 
-        assertThat(result).isEqualTo(0);
+        assertThat(result).isEqualTo(value);
     }
 
-    @Test
-    public void shouldReturnOneWhenOnePassed() throws Exception {
-        int result = testedObject.Add("1");
-
-        assertThat(result).isEqualTo(1);
-    }
-
-    @Test
-
-    public void shouldReturnSumWhenTwoNumbersPassed() {
-        int result = testedObject.Add("1,2");
-
-        assertThat(result).isEqualTo(3);
-    }
-
-    @Test
-
-    public void shouldReturnSumOfNumbersPassed(){
-        int result = testedObject.Add("2,3,15");
-
-        assertThat(result).isEqualTo(20);
-    }
 }
