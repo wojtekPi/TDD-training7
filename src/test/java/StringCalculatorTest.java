ff866b24524285eba8876b06396afe12bed92080
Marcin D <marcin.dziedzic02@gmail.com>
2017-12-09 14:37:10 +0100

    Add

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -1,53 +1,45 @@
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import junitparams.naming.TestCaseName;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * Tdd training on 09.12.17.
  */
-
+@RunWith(JUnitParamsRunner.class)
 public class StringCalculatorTest {
 
     StringCalculator testedObject;
 
     @Before
-    public void setUp(){
+    public void setUp() {
         testedObject = new StringCalculator();
+    }
+
+    private Object[][] parametersForTestingTwoNumbersInInput () {
+        return new Object [][]{
+                {"1,2", 3},
+                {"1,0", 1},
+                {"2,3,15", 20},
+                {"", 0}
+
+        };
+    }
+    @Test
+    @Parameters (method = "parametersForTestingNumbersInInput")
+    @TestCaseName("Should return {1} when {0} passed. ")
+    public void shouldReturnCorrectResultWhenNumbersPassed (String input, int expectedOutput) {
+        int result = testedObject.Add(input);
+
+        assertThat(result).isEqualTo(expectedOutput);
     }
 
     @Test
     public void shouldCreateObject() throws Exception {
         assertThat(testedObject).isNotNull();
-    }
-
-    @Test
-    public void shouldReturnZeroWhenEmptyStringPassed() {
-        int result = testedObject.Add("");
-
-        assertThat(result).isEqualTo(0);
-    }
-
-    @Test
-    public void shouldReturnOneWhenOnePassed() throws Exception {
-        int result = testedObject.Add("1");
-
-        assertThat(result).isEqualTo(1);
-    }
-
-    @Test
-
-    public void shouldReturnSumWhenTwoNumbersPassed() {
-        int result = testedObject.Add("1,2");
-
-        assertThat(result).isEqualTo(3);
-    }
-
-    @Test
-
-    public void shouldReturnSumOfNumbersPassed(){
-        int result = testedObject.Add("2,3,15");
-
-        assertThat(result).isEqualTo(20);
     }
 }
