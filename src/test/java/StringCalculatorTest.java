ce76b20707184466b49f94fe17c630fe62628e45
lemagamon <aga.a.mazanek@gmail.com>
2017-12-09 14:35:29 +0100

    param tests added

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -1,7 +1,11 @@
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import junitparams.naming.TestCaseName;
 import org.junit.Before;
 import org.junit.Test;
-
+import org.junit.runner.RunWith;
 import static org.assertj.core.api.Assertions.assertThat;
+@RunWith(JUnitParamsRunner.class)
 
 /**
  * Tdd training on 09.12.17.
@@ -21,33 +25,51 @@ public class StringCalculatorTest {
         assertThat(testedObject).isNotNull();
     }
 
-    @Test
-    public void shouldReturnZeroWhenEmptyStringPassed() {
-        int result = testedObject.Add("");
+//    @Test
+//    public void shouldReturnZeroWhenEmptyStringPassed() {
+//        int result = testedObject.Add("");
+//
+//        assertThat(result).isEqualTo(0);
+//    }
+//
+//    @Test
+//    public void shouldReturnOneWhenOnePassed() throws Exception {
+//        int result = testedObject.Add("1");
+//
+//        assertThat(result).isEqualTo(1);
+//    }
+//
+//    @Test
+//
+//    public void shouldReturnSumWhenTwoNumbersPassed() {
+//        int result = testedObject.Add("1,2");
+//
+//        assertThat(result).isEqualTo(3);
+//    }
+//
+//    @Test
+//
+//    public void shouldReturnSumOfNumbersPassed(){
+//        int result = testedObject.Add("2,3,15");
+//
+//        assertThat(result).isEqualTo(20);
+//    }
 
-        assertThat(result).isEqualTo(0);
+    private Object [][] parametersForTestingNumbersInput() {
+        return new Object[][] {
+                {"1,2", 3},
+                {"1,0", 1},
+                {"2,4,15", 21},
+                {"0", 0},
+                {"", 0}
+        };
     }
 
     @Test
-    public void shouldReturnOneWhenOnePassed() throws Exception {
-        int result = testedObject.Add("1");
-
-        assertThat(result).isEqualTo(1);
-    }
-
-    @Test
-
-    public void shouldReturnSumWhenTwoNumbersPassed() {
-        int result = testedObject.Add("1,2");
-
-        assertThat(result).isEqualTo(3);
-    }
-
-    @Test
-
-    public void shouldReturnSumOfNumbersPassed(){
-        int result = testedObject.Add("2,3,15");
-
-        assertThat(result).isEqualTo(20);
+    @Parameters(method = "parametersForTestingNumbersInput")
+    @TestCaseName("Should return {1} when {0} passed.") //opcjonalne
+    public void shouldReturnResultWhenNumbersPassed(String input, int expectedOutput) {
+        int result = testedObject.Add(input);
+        assertThat(result).isEqualTo(expectedOutput);
     }
 }
