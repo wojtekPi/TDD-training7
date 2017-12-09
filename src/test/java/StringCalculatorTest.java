f9fd247a4bd304b777905dea95dcd9d8b32c171e
mieschko <mieszko.kolodziej@gmail.com>
2017-12-09 14:42:08 +0100

    verbessert

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -1,12 +1,17 @@
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import junitparams.naming.TestCaseName;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * Tdd training on 09.12.17.
  */
-
+@RunWith(JUnitParamsRunner.class)
 public class StringCalculatorTest {
 
     StringCalculator testedObject;
@@ -20,34 +25,26 @@ public class StringCalculatorTest {
     public void shouldCreateObject() throws Exception {
         assertThat(testedObject).isNotNull();
     }
-
+    private Object[][] parametersForTestingTwoNumbersInInput(){
+        return new Object[][]{
+                {"1,2" , 3},
+                {"1,0" , 1},
+                {"1,2,3,4" , 10},
+                {"", 0},
+                {"1", 1}
+        };
+    }
     @Test
-    public void shouldReturnZeroWhenEmptyStringPassed() {
-        int result = testedObject.Add("");
+    @Parameters(method = "parametersForTestingTwoNumbersInInput")
+    @TestCaseName("Should return {1} when {0} passed.")
+    public void shouldReturnSum(String input, int expectedOutput) {
+        int result = testedObject.Add(input);
 
-        assertThat(result).isEqualTo(0);
+        assertThat(result).isEqualTo(expectedOutput);
     }
 
-    @Test
-    public void shouldReturnOneWhenOnePassed() throws Exception {
-        int result = testedObject.Add("1");
 
-        assertThat(result).isEqualTo(1);
-    }
 
-    @Test
 
-    public void shouldReturnSumWhenTwoNumbersPassed() {
-        int result = testedObject.Add("1,2");
 
-        assertThat(result).isEqualTo(3);
-    }
-
-    @Test
-
-    public void shouldReturnSumOfNumbersPassed(){
-        int result = testedObject.Add("2,3,15");
-
-        assertThat(result).isEqualTo(20);
-    }
 }
