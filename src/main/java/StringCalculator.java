d215e45c88ddc7b350e84a219fcd761fc406720b
lemagamon <aga.a.mazanek@gmail.com>
2017-12-09 12:31:42 +0100

    tests added

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,9 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class StringCalculator {
 
     public int Add(String numbers) {
+
+       String[] myNumbers = numbers.split(",");
+       int sum = 0;
+
         if(numbers.equals("1")){
             return 1;
+        } else if (numbers.equals("")){
+            return 0;
+        } else {
+            for (int i = 0; i < myNumbers.length; i++) {
+                Integer parsedNumber = Integer.valueOf(myNumbers[i]);
+                sum += parsedNumber;
+            }
         }
-        return 0;
+        return sum;
     }
 }
