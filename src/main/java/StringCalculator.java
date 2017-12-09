7e8c9ced8b00c2ee37dc55c0d6cdc15d63b7541a
Aneta <anetaryznar@gmail.com>
2017-12-09 12:13:34 +0100

    1

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,6 +1,22 @@
 public class StringCalculator {
 
     public int Add(String numbers) {
-        return 0;
+
+        String[] numbersArray = numbers.split(",");
+        Integer.valueOf(numbers);
+        int sum = 0;
+
+        for (int i = 0; i < numbersArray.length; i++) {
+            sum +=Integer.valueOf(numbersArray[i]);
+
+        }
+        return sum;
+
+//        if (numbers.equals("1")) {
+//            return 1;
+//        } else if (numbers.equals("1,2")) {
+//            return 3;
+//        }
+//        return 0;
     }
 }
