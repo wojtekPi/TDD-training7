74ab9f3f8ec7c745173e570f80c73907df880036
Rosiej <mateusz.rosiejka@gmail.com>
2017-12-09 13:13:29 +0100

    final separator and final empty string added

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,14 +1,17 @@
 public class StringCalculator {
 
+    public static final String SEPARATOR = ",";
+    public static final String EMPTY_STRING = "";
+
     public int Add(String numbers) {
         if (numbers.equals("1")) {
             return 1;
         }
-        if (numbers.equals("")||numbers.equals("0")){
+        if (numbers.equals(EMPTY_STRING)||numbers.equals("0")){
             return 0;
         }
 
-        String[] tempArr = numbers.split(",");
+        String[] tempArr = numbers.split(SEPARATOR);
 
         int[] numbersArr = new int[tempArr.length];
 
