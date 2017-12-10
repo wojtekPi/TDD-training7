b3a406626da5b4671e0edc2daeec0b5e6553c56f
Jaauba <jakub.bawol@gmail.com>
2017-12-10 09:57:36 +0100

    exception

--- src/main/java/bank/PaymentService.java
+++ src/main/java/bank/PaymentService.java
@@ -3,6 +3,8 @@ package bank;
 public class PaymentService {
 
     void transferMoney(Account from, Account to, int howMuch) {
+        if(from.amount < -500) {
+            throw new IllegalArgumentException("You don't have enough money");}
         from.amount -= howMuch;
         to.amount += howMuch;
     }
--- src/test/java/bank/PaymentServiceTest.java
+++ src/test/java/bank/PaymentServiceTest.java
@@ -4,7 +4,9 @@ import junitparams.JUnitParamsRunner;
 import junitparams.Parameters;
 import junitparams.naming.TestCaseName;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -31,6 +33,16 @@ public class PaymentServiceTest {
         };
     }
 
+    private Object[][] partametersForTestingException() {
+        return new Object[][] {
+                {100, 30, 80, 50, 20},
+                {0, 10, -50, 60, 50},
+                {200, 100, 200, 100, 0},
+                {-50, -30, -80, 0, 30},
+        };
+    }
+
+
     @Test
     @Parameters(method = "partametersForTestingInInput")
     @TestCaseName("Should return {3} when {4} passed")
@@ -44,6 +56,24 @@ public class PaymentServiceTest {
 
         assertThat(bankAccountFrom.amount).isEqualTo(amountFromAfter);
         assertThat(bankAccountTo.amount).isEqualTo(amountToAfter);
+    }
+
+    @Rule
+    public ExpectedException thrown= ExpectedException.none();
+    @Test
+    public void shouldThrowExecptionWhenBalanaceIsTooSmall() {
+        ;
+        bankAccountFrom = new Account(-600);
+        bankAccountTo = new Account(1000);
+
+
+        thrown.expect(IllegalArgumentException.class);
+        thrown.expectMessage("You don't have enough money");
+        paymentService.transferMoney(bankAccountFrom, bankAccountTo, 300);
+
+
+
+
     }
 
 }
