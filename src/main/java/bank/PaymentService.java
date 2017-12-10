ca44fd06cfb86a0a752e91215bd541a6dc70166f
MacBook Pro Magda <magda@ssolutions.com.pl>
2017-12-10 11:50:54 +0100

    Payment services with Testing exceptions and currencies (done).

--- src/main/java/bank/PaymentService.java
+++ src/main/java/bank/PaymentService.java
@@ -3,17 +3,17 @@ package bank;
 public class PaymentService {
 
     void transferMoney(Account from, Account to, Instrument instrument)  {
-        if(from.instrument.amount >= -500 && checkCurrencies(from, to, instrument)) {
+        checkCurrencies(from, to, instrument);
+        if(from.instrument.amount >= -500) {
             from.instrument.amount -= instrument.amount;
             to.instrument.amount += instrument.amount;
         } else throw new IllegalArgumentException("I'm very sorry, but you don't have enough money...");
     }
 
-    boolean checkCurrencies(Account from, Account to, Instrument instrument) {
+    void checkCurrencies(Account from, Account to, Instrument instrument) {
         if(from.instrument.curency == to.instrument.curency &&
                 from.instrument.curency == instrument.curency &&
-                to.instrument.curency == instrument.curency) return true;
+                to.instrument.curency == instrument.curency) return;
         else throw new IllegalArgumentException("I'm very sorry, incorrect currencies...");
-
     }
 }
